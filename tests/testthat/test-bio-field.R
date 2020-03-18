d7c5d027952b1c7ffbe4c77cefee39df450c2d5e
kenhanscombe <ken.hanscombe@gmail.com>
2020-03-18 09:54:21 +0000

    [Add] Pass phenotype subdirectory to bio_field
    Change approach taken in last commit: instead of removing the phenotype
    subdirectory argument to bio_phen, keep and pass the argument on the to
    the call to bio_field. Allows bio_phen to be supplied a subdirectory
    containing required data (like all other functionality). Also update
    field test to take subdirectory argument.

--- tests/testthat/test-bio-field.R
+++ tests/testthat/test-bio-field.R
@@ -8,7 +8,7 @@ test_that("bio_field stops and prints error for invalid path", {
 
 test_that("bio_field creates valid path to csv", {
   testdata_path <- system.file("testdata", package = "ukbkings")
-  df <- bio_field(testdata_path)
+  df <- bio_field(testdata_path, "")
 
   expect_match(df$basket[1], "^ukb[0-9]+$")
   expect_true(file.exists(df$path[1]))
