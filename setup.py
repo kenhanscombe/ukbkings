208c242d325516de1b21d6883fe2fb8b50d1489e
kenhanscombe <ken.hanscombe@gmail.com>
2020-04-24 16:35:49 +0100

    Add setup.py to create directory structure
    setup.py create required project subdirectories, collects required resources, downloads Snakefile and associated scripts.

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+import os
+
+os.system(
+  """
+  mkdir src
+  mkdir resources
+  mkdir log
+
+  wget https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/Snakefile
+
+  wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/munge_ukb.py
+  wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/cluster.json
+  wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/snake.py
+  
+  wget -P resources/ http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
+  wget -P resources/ -nd biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
+  wget -P resources/ -nd  biobank.ndph.ox.ac.uk/showcase/util/ukbunpack
+  wget -P resources/ -nd  biobank.ndph.ox.ac.uk/showcase/util/ukbconv
+  
+  chmod +x snake.py
+  chmod +x resources/ukb*
+  """)
