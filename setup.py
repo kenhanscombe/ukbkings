d6f029879fed40476e9f3c7f9dbef60b6677e57b
kenhanscombe <ken.hanscombe@gmail.com>
2020-04-27 10:43:06 +0100

    Move snake.py to project root
    src/ only contains scripts called by the Snakefile; setup.py, snake.py and Snakefile are at project root. Also, make src/munge_ukb.py executable.

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.system(
 
   wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/munge_ukb.py
   wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/cluster.json
-  wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/snake.py
+  wget https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/snake.py
   
   wget -P resources/ http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
   wget -P resources/ -nd biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
@@ -19,5 +19,6 @@ os.system(
   wget -P resources/ -nd  biobank.ndph.ox.ac.uk/showcase/util/ukbconv
   
   chmod +x snake.py
+  chmod +x src/munge_ukb.py
   chmod +x resources/ukb*
   """)
