4d78b93db44cf73940cff454d5902b972a150029
kenhanscombe <ken.hanscombe@gmail.com>
2020-05-12 14:37:42 +0100

    Fix optional dry-run

--- snake.py
+++ snake.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python
 
 import os
+import argparse
 
 if __name__ == "__main__":
-  os.system('snakemake \
-             --jobs 99 \
-             --cluster-config src/cluster.json \
-             --use-envmodules \
-             --cluster \
-              "sbatch \
-                --job-name={cluster.name} \
-                --partition={cluster.partition} \
-                --error={cluster.error} \
-                --out={cluster.out} \
-                --time {cluster.time} \
-                --mem=10G" -n')
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-n', '--dry-run', action='store_true',
+                      help='Dry-run?')
+  args = parser.parse_args()
+  
+  snakemake_call = 'snakemake \
+                    --jobs 99 \
+                    --cluster-config src/cluster.json \
+                    --use-envmodules \
+                    --cluster \
+                      "sbatch \
+                       --job-name={cluster.name} \
+                       --partition={cluster.partition} \
+                       --error={cluster.error} \
+                       --out={cluster.out} \
+                       --time {cluster.time} \
+                       --mem=10G"'
+  if args.dry_run:
+    os.system(snakemake_call + ' -n')
+  else:
+    os.system(snakemake_call)
