c7d587d126f86cd2beadf209fc7047c9747663f6
kenhanscombe <ken.hanscombe@gmail.com>
2020-05-12 13:24:34 +0100

    Overwrite existing files

--- setup.py
+++ setup.py
@@ -7,16 +7,16 @@ os.system(
   mkdir resources
   mkdir log
 
-  wget https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/Snakefile
+  wget -O Snakefile https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/Snakefile
 
-  wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/munge_ukb.py
-  wget -P src/ https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/cluster.json
-  wget https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/snake.py
+  wget -P src/ -O munge_ukb.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/munge_ukb.py
+  wget -P src/ -O cluster.json https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/cluster.json
+  wget -O snake.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/snake.py
   
-  wget -P resources/ http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
-  wget -P resources/ -nd biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
-  wget -P resources/ -nd  biobank.ndph.ox.ac.uk/showcase/util/ukbunpack
-  wget -P resources/ -nd  biobank.ndph.ox.ac.uk/showcase/util/ukbconv
+  wget -P resources/ -O Codings_Showcase.csv http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
+  wget -P resources/ -nd -O encoding.ukb biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
+  wget -P resources/ -nd -O ukbunpack biobank.ndph.ox.ac.uk/showcase/util/ukbunpack
+  wget -P resources/ -nd -O ukbconv biobank.ndph.ox.ac.uk/showcase/util/ukbconv
   
   chmod +x snake.py
   chmod +x src/munge_ukb.py
