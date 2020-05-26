42e16e39c8e564aa3c5984194ab5dc099adc53a7
kenhanscombe <ken.hanscombe@gmail.com>
2020-05-26 11:53:24 +0100

    Update links

--- link.py
+++ link.py
@@ -18,20 +18,20 @@ def create_links(project, fam, sample):
         mkdir genotyped
         mkdir imputed
 
-        ln -S fam genotyped/ukb{ukbid}.fam
-        ln -S sample imputed/ukb{ukbid}.sample
-        ln -S {imputed}ukb_sqc_v2.txt imputed/ukb_sqc.txt
-        ln -S {imputed}ukb_sqc_v2_fields.txt imputed/ukb_sqc_fields.txt
+        ln -s fam genotyped/ukb{ukbid}.fam
+        ln -s sample imputed/ukb{ukbid}.sample
+        ln -s {imputed}ukb_sqc_v2.txt imputed/ukb_sqc.txt
+        ln -s {imputed}ukb_sqc_v2_fields.txt imputed/ukb_sqc_fields.txt
 
 
-        ln -S {genotyped}ukb_binary_v2.bed genotyped/ukb{ukbid}.bed
-        ln -S {genotyped}ukb_binary_v2.bed genotyped/ukb{ukbid}.bim
+        ln -s {genotyped}ukb_binary_v2.bed genotyped/ukb{ukbid}.bed
+        ln -s {genotyped}ukb_binary_v2.bed genotyped/ukb{ukbid}.bim
 
         for i in X XY 1 .. 22
         do
-        ln -S {imputed}ukb_imp_chr"$i"_v3.bgen imputed/ukb_imp_chr"$i".bgen
-        ln -S {imputed}ukb_imp_chr"$i"_v3.bgen.bgi imputed/ukb_imp_chr"$i".bgen.bgi
-        ln -S {imputed}ukb_mfi_chr"$i"_v3.txt imputed/ukb_mfi_chr"$i".txt
+        ln -s {imputed}ukb_imp_chr"$i"_v3.bgen imputed/ukb_imp_chr"$i".bgen
+        ln -s {imputed}ukb_imp_chr"$i"_v3.bgen.bgi imputed/ukb_imp_chr"$i".bgen.bgi
+        ln -s {imputed}ukb_mfi_chr"$i"_v3.txt imputed/ukb_mfi_chr"$i".txt
         done
         """)
 
