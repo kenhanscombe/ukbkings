f5339e0b964987cd4a8db813cdfec10554ae38c2
kenhanscombe <ken.hanscombe@gmail.com>
2020-05-26 11:22:18 +0100

    Get link.py script

--- setup.py
+++ setup.py
@@ -25,6 +25,7 @@ os.system(
     wget -P src/ -O src/munge_ukb.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/munge_ukb.py
     wget -P src/ -O src/cluster.json https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/cluster.json
     wget -O snake.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/snake.py
+    wget -O link.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/link.py
 
     wget -P resources/ -O resources/Codings_Showcase.csv http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
     wget -P resources/ -nd -O resources/encoding.ukb biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
