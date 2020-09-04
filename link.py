83aec4232f8ceb63f8beea298225f147f788e038
kenhanscombe <ken.hanscombe@gmail.com>
2020-09-04 11:30:00 +0100

    Re-write project directory setup
    project.py makes project directory structure, downloads UKB
    utilities, makes UKB programs executable, adds softlinks to
    genotyped and imputed genetic data. link.py is a CLI that allows
    the addition of softlinks to genetic sample data: .fam, .sample files

--- link.py
+++ link.py
@@ -1,51 +1,20 @@
 #!/usr/bin/env python3
 
+import click
+from project import Project
 
-def create_links(project, fam, sample):
-    """Creates symbolic links to genotyped and imputed genetic data.
 
-    Args:
-        project (str): ukb project directory
-        fam (str): path to fam file
-        sample (str): path to sample file
+@click.command()
+@click.option('--ukbid', help='UKB project id, e.g., 12345.')
+@click.option('--fam', help='Path to .fam file.')
+@click.option('--sample', help='Path to .sample file.')
+def link_sample(ukbid, fam, sample):
+    """Adds softlinks to genetic sample data.
     """
-    ukbid = re.sub('^.*biobank/ukb|_.*$', '', project)
-    genotyped = '/scratch/datasets/ukbiobank/June2017/Genotypes/'
-    imputed = '/scratch/datasets/ukbiobank/June2017/Imputed/'
-
-    os.system(
-        f"""
-        mkdir genotyped
-        mkdir imputed
-
-        ln -s {fam} genotyped/ukb{ukbid}.fam
-        ln -s {sample} imputed/ukb{ukbid}.sample
-        ln -s {imputed}ukb_sqc_v2.txt imputed/ukb_sqc.txt
-        ln -s {imputed}ukb_sqc_v2_fields.txt imputed/ukb_sqc_fields.txt
-
-
-        ln -s {genotyped}ukb_binary_v2.bed genotyped/ukb{ukbid}.bed
-        ln -s {genotyped}ukb_binary_v2.bed genotyped/ukb{ukbid}.bim
-
-        for i in X XY $(seq 1 22)
-        do
-        ln -s {imputed}ukb_imp_chr"$i"_v3.bgen imputed/ukb_imp_chr"$i".bgen
-        ln -s {imputed}ukb_imp_chr"$i"_v3.bgen.bgi imputed/ukb_imp_chr"$i".bgen.bgi
-        ln -s {imputed}ukb_mfi_chr"$i"_v3.txt imputed/ukb_mfi_chr"$i".txt
-        done
-        """)
+    p = Project()
+    p.link_genetics(ukbid=ukbid, fam=fam, sample=sample,
+                    initialized=True)
 
 
 if __name__ == '__main__':
-    import os
-    import re
-    import argparse
-
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-p', '--project', help='ukb project directory')
-    parser.add_argument('-f', '--fam', help='path to fam file')
-    parser.add_argument('-s', '--sample', help='path to sample file')
-
-    args = parser.parse_args()
-
-    create_links(args.project, args.fam, args.sample)
+    link_sample()
---
+++ project.py
@@ -0,0 +1,76 @@
+#!/usr/bin/env python3
+
+import os
+import re
+import click
+
+
+class Project:
+    def __init__(self):
+        """Sets up the UKB project directory for use with ukbkings.
+        """
+        os.system('''
+            mkdir src
+            mkdir resources
+            mkdir log
+
+            wget -O Snakefile https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/Snakefile
+
+            wget -P src/ -O src/munge_ukb.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/munge_ukb.py
+            wget -P src/ -O src/cluster.json https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/cluster.json
+            wget -O snake.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/snake.py
+            wget -O link.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/link.py
+
+            wget -P resources/ -O resources/Codings_Showcase.csv http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
+            wget -P resources/ -nd -O resources/encoding.ukb biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
+            wget -P resources/ -nd -O resources/ukbunpack biobank.ndph.ox.ac.uk/showcase/util/ukbunpack
+            wget -P resources/ -nd -O resources/ukbconv biobank.ndph.ox.ac.uk/showcase/util/ukbconv
+
+            chmod +x snake.py
+            chmod +x link.py
+            chmod +x src/munge_ukb.py
+            chmod +x resources/ukb*''')
+
+    def link_genetics(self, ukbid, fam, sample, initialized=False):
+        """
+        Creates symbolic links to genotyped and imputed genetic data.
+
+        Args:
+            ukbid (str): ukb project id
+            fam (str): path to fam file
+            sample (str): path to sample file
+        """
+        # ukbid = re.sub('^.*biobank/ukb|_.*$', '', project)
+        genotyped = '/scratch/datasets/ukbiobank/June2017/Genotypes/'
+        imputed = '/scratch/datasets/ukbiobank/June2017/Imputed/'
+
+        if not initialized:
+            os.system(
+                f'''
+                mkdir genotyped
+                mkdir imputed
+
+                ln -s {imputed}ukb_sqc_v2.txt imputed/ukb_sqc.txt
+                ln -s {imputed}ukb_sqc_v2_fields.txt imputed/ukb_sqc_fields.txt
+
+                ln -s {genotyped}ukb_binary_v2.bed genotyped/ukb{ukbid}.bed
+                ln -s {genotyped}ukb_binary_v2.bim genotyped/ukb{ukbid}.bim
+
+                for i in X XY $(seq 1 22)
+                do
+                ln -s {imputed}ukb_imp_chr"$i"_v3.bgen imputed/ukb_imp_chr"$i".bgen
+                ln -s {imputed}ukb_imp_chr"$i"_v3.bgen.bgi imputed/ukb_imp_chr"$i".bgen.bgi
+                ln -s {imputed}ukb_mfi_chr"$i"_v3.txt imputed/ukb_mfi_chr"$i".txt
+                done
+                ''')
+
+        if fam:
+            os.system(f'ln -s {fam} genotyped/ukb{ukbid}.fam')
+
+        if sample:
+            os.system(f'ln -s {sample} imputed/ukb{ukbid}.sample')
+
+
+if __name__ == "__main__":
+    project = Project()
+    project.link_genetics()
