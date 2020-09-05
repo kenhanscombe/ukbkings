02f0f7cce6a82c5e1c5343313f3a8b98ed7568ea
kenhanscombe <ken.hanscombe@gmail.com>
2020-09-05 18:22:51 +0100

    Change wget utilities back to master

--- project.py
+++ project.py
@@ -15,12 +15,12 @@ def build_project():
         rm -rf resources; mkdir resources
         rm -rf log; mkdir log
 
-        wget -O Snakefile https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/Snakefile
+        wget -O Snakefile https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/Snakefile
 
-        wget -P src/ -O src/munge_ukb.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/src/munge_ukb.py
-        wget -P src/ -O src/cluster.json https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/src/cluster.json
-        wget -O snake.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/snake.py
-        wget -O link.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/link.py
+        wget -P src/ -O src/munge_ukb.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/munge_ukb.py
+        wget -P src/ -O src/cluster.json https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/src/cluster.json
+        wget -O snake.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/snake.py
+        wget -O link.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/master/link.py
 
         wget -P resources/ -O resources/Codings_Showcase.csv http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
         wget -P resources/ -nd -O resources/encoding.ukb biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
