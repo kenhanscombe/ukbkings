f25fe5f9c7b571eaba42bf7887505dab2a1b967e
kenhanscombe <ken.hanscombe@gmail.com>
2020-09-05 14:40:24 +0100

    Remove Project class
    Re-wrote class methods as two separate functions. Building a project
    directory with class __init__ meant the CLI link.py which called
    the method link_genetics rebuilt the directory on instantiating an
    instance of Project.

--- link.py
+++ link.py
@@ -1,17 +1,19 @@
 #!/usr/bin/env python3
 
+import os
 import click
+
 from project import Project
 
 
 @click.command()
-@click.option('--fam', help='Path to .fam file.')
-@click.option('--sample', help='Path to .sample file.')
+@click.option('-f', '--fam', help='Path to .fam file.')
+@click.option('-s', '--sample', help='Path to .sample file.')
 def link_sample(fam, sample):
     """Adds softlinks to genetic sample data.
     """
-    p = Project()
-    p.link_genetics(fam=fam, sample=sample, initialized=True)
+    link_genetics(fam=fam, sample=sample, initialized=True)
+    os.system('rm -rf __pycache__')
 
 
 if __name__ == '__main__':
--- project.py
+++ project.py
@@ -7,74 +7,72 @@ import click
 from pathlib import Path
 
 
-class Project:
-    def __init__(self):
-        """Sets up the UKB project directory for use with ukbkings.
-        """
-        os.system('''
-            mkdir src
-            mkdir resources
-            mkdir log
+def build_project():
+    """Sets up the UKB project directory for use with ukbkings.
+    """
+    os.system('''
+        mkdir src
+        mkdir resources
+        mkdir log
 
-            wget -O Snakefile https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/Snakefile
+        wget -O Snakefile https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/Snakefile
 
-            wget -P src/ -O src/munge_ukb.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/src/munge_ukb.py
-            wget -P src/ -O src/cluster.json https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/src/cluster.json
-            wget -O snake.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/snake.py
-            wget -O link.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/link.py
+        wget -P src/ -O src/munge_ukb.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/src/munge_ukb.py
+        wget -P src/ -O src/cluster.json https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/src/cluster.json
+        wget -O snake.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/snake.py
+        wget -O link.py https://raw.githubusercontent.com/kenhanscombe/ukbkings/link-genetics/link.py
 
-            wget -P resources/ -O resources/Codings_Showcase.csv http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
-            wget -P resources/ -nd -O resources/encoding.ukb biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
-            wget -P resources/ -nd -O resources/ukbunpack biobank.ndph.ox.ac.uk/showcase/util/ukbunpack
-            wget -P resources/ -nd -O resources/ukbconv biobank.ndph.ox.ac.uk/showcase/util/ukbconv
+        wget -P resources/ -O resources/Codings_Showcase.csv http://biobank.ctsu.ox.ac.uk/~bbdatan/Codings_Showcase.csv
+        wget -P resources/ -nd -O resources/encoding.ukb biobank.ctsu.ox.ac.uk/crystal/util/encoding.ukb
+        wget -P resources/ -nd -O resources/ukbunpack biobank.ndph.ox.ac.uk/showcase/util/ukbunpack
+        wget -P resources/ -nd -O resources/ukbconv biobank.ndph.ox.ac.uk/showcase/util/ukbconv
 
-            chmod +x snake.py
-            chmod +x link.py
-            chmod +x src/munge_ukb.py
-            chmod +x resources/ukb*''')
+        chmod +x snake.py
+        chmod +x link.py
+        chmod +x src/munge_ukb.py
+        chmod +x resources/ukb*''')
 
-    def link_genetics(self, fam=None, sample=None, initialized=False):
-        """
-        Creates symbolic links to genotyped and imputed genetic data.
 
-        Args:
-            fam (str): path to fam file
-            sample (str): path to sample file
-        """
-        p = Path('.')
-        wd_basename = p.absolute().name
-        project_id = re.sub('ukb|_.*$', '', wd_basename)
-        # ukbid = re.sub('^.*biobank/ukb|_.*$', '', project)
-        genotyped = '/scratch/datasets/ukbiobank/June2017/Genotypes/'
-        imputed = '/scratch/datasets/ukbiobank/June2017/Imputed/'
+def link_genetics(fam=None, sample=None, initialized=False):
+    """
+    Creates symbolic links to genotyped and imputed genetic data.
 
-        if not initialized:
-            os.system(
-                f'''
-                rm -rf genotyped; mkdir genotyped
-                rm -rf imputed; mkdir imputed
+    Args:
+        fam (str): path to fam file
+        sample (str): path to sample file
+    """
+    p = Path('.')
+    wd_basename = p.absolute().name
+    project_id = re.sub('ukb|_.*$', '', wd_basename)
+    genotyped = '/scratch/datasets/ukbiobank/June2017/Genotypes/'
+    imputed = '/scratch/datasets/ukbiobank/June2017/Imputed/'
 
-                ln -s {imputed}ukb_sqc_v2.txt imputed/ukb_sqc.txt
-                ln -s {imputed}ukb_sqc_v2_fields.txt imputed/ukb_sqc_fields.txt
+    if not initialized:
+        os.system(f'''
+            rm -rf genotyped; mkdir genotyped
+            rm -rf imputed; mkdir imputed
 
-                ln -s {genotyped}ukb_binary_v2.bed genotyped/ukb{project_id}.bed
-                ln -s {genotyped}ukb_binary_v2.bim genotyped/ukb{project_id}.bim
+            ln -s {imputed}ukb_sqc_v2.txt imputed/ukb_sqc.txt
+            ln -s {imputed}ukb_sqc_v2_fields.txt imputed/ukb_sqc_fields.txt
 
-                for i in X XY $(seq 1 22)
-                do
-                ln -s {imputed}ukb_imp_chr"$i"_v3.bgen imputed/ukb_imp_chr"$i".bgen
-                ln -s {imputed}ukb_imp_chr"$i"_v3.bgen.bgi imputed/ukb_imp_chr"$i".bgen.bgi
-                ln -s {imputed}ukb_mfi_chr"$i"_v3.txt imputed/ukb_mfi_chr"$i".txt
-                done
-                ''')
+            ln -s {genotyped}ukb_binary_v2.bed genotyped/ukb{project_id}.bed
+            ln -s {genotyped}ukb_binary_v2.bim genotyped/ukb{project_id}.bim
 
-        if fam:
-            os.system(f'ln -s {fam} genotyped/ukb{project_id}.fam')
+            for i in X XY $(seq 1 22)
+            do
+            ln -s {imputed}ukb_imp_chr"$i"_v3.bgen imputed/ukb_imp_chr"$i".bgen
+            ln -s {imputed}ukb_imp_chr"$i"_v3.bgen.bgi imputed/ukb_imp_chr"$i".bgen.bgi
+            ln -s {imputed}ukb_mfi_chr"$i"_v3.txt imputed/ukb_mfi_chr"$i".txt
+            done
+            ''')
 
-        if sample:
-            os.system(f'ln -s {sample} imputed/ukb{project_id}.sample')
+    if fam:
+        os.system(f'ln -s {fam} genotyped/ukb{project_id}.fam')
+
+    if sample:
+        os.system(f'ln -s {sample} imputed/ukb{project_id}.sample')
 
 
 if __name__ == "__main__":
-    project = Project()
-    project.link_genetics()
+    build_project()
+    link_genetics()
