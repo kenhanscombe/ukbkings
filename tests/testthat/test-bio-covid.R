48a7c21d098d08bfa5f87a8c8c3f6910498f4b23
kenhanscombe <ken.hanscombe@gmail.com>
2020-09-22 08:16:15 +0100

    Update bio_covid arguments in test

--- tests/testthat/test-bio-covid.R
+++ tests/testthat/test-bio-covid.R
@@ -1,8 +1,8 @@
-context('COVID-19 data')
+context('covid-19')
 
 test_that("bio_covid stops with error if no covid-19 data available", {
   expect_error(
-    bio_covid(".", data = "results", results_dir = ".", code_dir = "."),
+    bio_covid(".", data = "results", covid_dir = ".", code_dir = "."),
     "COVID-19 data is not available for this project."
   )
 })
