5ed61c6969ebc8eaff4af57a227e1af5ee3830d4
kenhanscombe <ken.hanscombe@gmail.com>
2020-10-04 14:17:53 +0100

    Update test with new bio_code args

--- tests/testthat/test-bio-code.R
+++ tests/testthat/test-bio-code.R
@@ -5,8 +5,10 @@ tmp <- testthat::setup(tempfile(tmpdir = tmp_dir))
 
 test_that("bio_code stops and prints error when no coding data available", {
   expect_error(
-    bio_code("./", code_dir = tmp_dir),
-    stringr::str_interp(c("Required file .* does not exist."))
+    bio_code(code_dir = "."),
+    stringr::str_interp(
+      c("Required file .*Codings_Showcase.csv does not exist.")
+    )
   )
 })
 
