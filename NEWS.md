af0ec537d60a9cd2f4a514f2aae66702d19a0cfc
kenhanscombe <ken.hanscombe@gmail.com>
2020-10-12 11:00:13 +0100

    Add read functions for COVID-19 GP data

--- NEWS.md
+++ NEWS.md
@@ -1,28 +1,43 @@
 # ukbkings (development version)
 
-**Note. All paths mentioned in the Changelog are relative to /scratch/datasets/ukbiobank/\<*project_dir*\>/.**
+**Note. All paths mentioned in the Changelog are relative to
+/scratch/datasets/ukbiobank/\<*project_dir*\>/.**
 
 * `bio_gen_ls` lists project genetic directory contents
 
-* `bio_code` updated path to resources/, which includes Codings_Showcase.csv
+* `bio_code` updated path to resources/, which includes
+Codings_Showcase.csv
+
+* `bio_covid` now also reads "Primary Care Data for COVID-19 Research":
+TPP and EMIS prescriptions and GP (clinical) data
 
 
 # ukbkings 0.1
 
 **24.09.2020 Ken B. Hanscombe**
 
-* Updated documentation for project setup and data access can be found under the Articles tab on the [ukbkings page](https://kenhanscombe.github.io/ukbkings/). These can also be read as vignettes within the installed R package
+* Updated documentation for project setup and data access can be found
+under the Articles tab on the
+[ukbkings page](https://kenhanscombe.github.io/ukbkings/). These can
+also be read as vignettes within the installed R package
 
 *New/updated functionality*
 
-* `bio_covid` now returns additional codings (corresponding to new columns in the results data), and the new blood group dataset.
+* `bio_covid` now returns additional codings (corresponding to new
+columns in the results data), and the new blood group dataset.
 
-* `bio_hesin` reads HES in-patient record-level data if available for the project.
+* `bio_hesin` reads HES in-patient record-level data if available for
+the project.
 
 * `bio_death` reads death record data if available for the project.
 
 *Genetic data*
 
-* Symlinks to genotyped and imputed genetic data are included in genotyped/ and imputed/ respectively. Project managers can add symlinks to fam, sample and relatedness files with link.py (see `python ./link.py --help` and the Article "Setup a UKB project on Rosalind").
+* Symlinks to genotyped and imputed genetic data are included in
+genotyped/ and imputed/ respectively. Project managers can add symlinks
+to fam, sample and relatedness files with link.py (see
+`python ./link.py --help` and the Article "Setup a UKB project on
+Rosalind").
 
-* resources/ now includes a the UKB program `ukbgene` used to retrieve UKB "link" files (.fam, .sample) and relatedness data.
+* resources/ now includes a the UKB program `ukbgene` used to retrieve
+UKB "link" files (.fam, .sample) and relatedness data.
--- R/bio_read.R
+++ R/bio_read.R
@@ -6,10 +6,14 @@ utils::globalVariables(c("ukb_type", "basket", "field", "path", "name",
 
 #' Reads project-specific UKB field codes
 #'
-#' The field code lookup table associates with each field id, a desciptive name (which includes the field id, array, index), ukb_type, r_type, basket and the path to the csv file containing the field data.
+#' The field code lookup table associates with each field id, a
+#' desciptive name (which includes the field id, array, index),
+#' ukb_type, r_type, basket and the path to the csv file containing the
+#' field data.
 #'
 #' @param project_dir Path to the enclosing directory of a UKB project
-#' @param pheno_dir Path to the enclosing directory of the phenotype data.
+#' @param pheno_dir Path to the enclosing directory of the phenotype
+#' data.
 #'
 #' @return A dataframe with columns: ukb_type, r_type, path
 #'
@@ -67,11 +71,16 @@ bio_field <- function(project_dir, pheno_dir = "phenotypes") {
 #' dataframe to a .rds file.
 #'
 #' @param project_dir Path to the enclosing directory of a UKB project.
-#' @param field_subset_file A path to a one-per-line text file of fields (no header).
-#' @param pheno_dir Path to the enclosing directory of the phenotype data.
-#' @param out Name of phenotype subset file. Default "ukb_phenotype_subset", writes ukb_phenotype_subset.rds to the current directory.
+#' @param field_subset_file A path to a one-per-line text file of
+#' fields (no header).
+#' @param pheno_dir Path to the enclosing directory of the phenotype
+#' data.
+#' @param out Name of phenotype subset file. Default
+#' "ukb_phenotype_subset", writes ukb_phenotype_subset.rds to the
+#' current directory.
 #'
-#' @details Read the serialized dataframe with readRDS("<name_of_phenotype_subset_file>.rds")
+#' @details Read the serialized dataframe with
+#' readRDS("<name_of_phenotype_subset_file>.rds")
 #'
 #' @importFrom data.table fread getDTthreads
 #' @importFrom dplyr pull filter group_by mutate na_if
@@ -150,10 +159,14 @@ bio_phen <- function(project_dir, field_subset_file,
 
 #' Adds \strong{field} column entries from a dataframe to a file
 #'
-#' @param data A dataframe with obligatory column \strong{field}. (Ideally the output of \code{\link{bio_field}}.)
-#' @param out Field subset file name (including path). Default "ukb_field_subset", writes or appends fields one per line to "ukb_field_subset" in the current directory.
+#' @param data A dataframe with obligatory column \strong{field}.
+#' (Ideally the output of \code{\link{bio_field}}.)
+#' @param out Field subset file name (including path). Default
+#' "ukb_field_subset", writes or appends fields one per line to
+#' "ukb_field_subset" in the current directory.
 #'
-#' @return A file with one field per line (no header). If the file exists, the additional fields are appended.
+#' @return A file with one field per line (no header). If the file
+#' exists, the additional fields are appended.
 #'
 #' @importFrom dplyr pull
 #' @export
@@ -172,13 +185,17 @@ bio_field_add <- function(data, out = "ukb_field_subset.txt") {
 
 #' Updates column names
 #'
-#' Renames a dataframe with UKB fields as column names with descriptive names.
+#' Renames a dataframe with UKB fields as column names with descriptive
+#' names.
 #'
-#' @param data A dataframe with UKB fields as column names. See \code{\link{bio_phen}}.
-#' @param field_finder A dataframe including a column of UKB fields and a column of descriptive names. See \code{\link{bio_field}}.
+#' @param data A dataframe with UKB fields as column names. See
+#' \code{\link{bio_phen}}.
+#' @param field_finder A dataframe including a column of UKB fields and
+#' a column of descriptive names. See \code{\link{bio_field}}.
 #'
-#' @return A dataframe with UKB field column names replaced with descriptive column names.
-#' importFrom dplyr filter rename
+#' @return A dataframe with UKB field column names replaced with
+#' descriptive column names.
+#' @importFrom dplyr filter rename
 #' @seealso \code{\link{bio_phen}}, \code{\link{bio_field}}
 #' @export
 bio_rename <- function(data, field_finder) {
@@ -193,7 +210,8 @@ bio_rename <- function(data, field_finder) {
 
 #' Reads the UKB showcase codings for categorical variables
 #'
-#' @param code_dir Path to the enclosing directory of the Codings_Showcase.csv.
+#' @param code_dir Path to the enclosing directory of the
+#' Codings_Showcase.csv.
 #'
 #' @return A dataframe with header Coding, Value, Meaning
 #'
@@ -216,15 +234,22 @@ bio_code <- function(code_dir = "/scratch/datasets/ukbiobank/resources") {
 
 #' Reads the primary care data
 #'
-#' Detailed patient level diagnoses, prescriptions, etc. Only available if these data have been requested for the particular project you have access to.
+#' Detailed patient level diagnoses, prescriptions, etc. Only available
+#' if these data have been requested for the particular project you
+#' have access to.
 #'
 #' @param project_dir Path to the enclosing directory of a UKB project.
-#' @param gp_dir Path to the enclosing directory of the primary care data.
-#' @param record A string specifying which primary care records are required: "clinical", "registrations", "scripts".
+#' @param gp_dir Path to the enclosing directory of the primary care
+#' data.
+#' @param record A string specifying which primary care records are
+#' required: "clinical", "registrations", "scripts".
 #'
-#' @return A dataframe. \strong{Note}. clinical data has 123,669,371 rows and 8 columns; registrations data has 361,841 rows and 4 columns; scripts data has 57,709,810 rows and 8 columns.
+#' @return A dataframe. \strong{Note}. clinical data has 123,669,371
+#' rows and 8 columns; registrations data has 361,841 rows and 4
+#' columns; scripts data has 57,709,810 rows and 8 columns.
 #'
-#' @seealso \href{http://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=3001}{Category 3001},
+#' @seealso
+#' \href{http://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=3001}{Category 3001},
 #' \href{http://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=591}{Resource 591},
 #' \href{http://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=592}{Resource 592}
 #'
@@ -254,16 +279,27 @@ bio_gp <- function(project_dir, record, gp_dir = "raw/") {
 
 #' Reads the COVID-19 data
 #'
-#' @description Record-level information for COVID-19 testing. Only available if these data have been requested for the particular project you have access to.
+#' @description Record-level information for COVID-19 testing. Only
+#' available if these data have been requested for the particular
+#' project you have access to.
 #'
 #' @param project_dir Path to the enclosing directory of a UKB project.
-#' @param data A string specifying data required: "results" returns COVID-19 test results; "misc" returns blood group extracted from imputed genotype; "codes" returns the code meanings for the categorical variables.
-#' @param covid_dir Path to the enclosing directory of the covid19_results.txt
-#' @param code_dir Path to the enclosing directory of the data coding files described in the UKB showcase notes under \href{http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=40100}{data field 40100}.
+#' @param data A string specifying data required: "results", "misc",
+#' "tppscripts", "tppclinical", "emisscripts", "emisclinical", "codes".
+#' @param covid_dir Path to the enclosing directory of the
+#' covid19_results.txt
+#' @param code_dir Path to the enclosing directory of the data coding
+#' files described in the UKB showcase notes under
+#' \href{http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=40100}{data field 40100}.
 #'
-#' @return Returns a dataframe of either the COVID-19 testing results, blood group, or codes associated with fields in the results dataframe, depending on the value of argument `data`.
+#' @return Returns a dataframe of either the COVID-19 testing results,
+#' blood group, or codes associated with fields in the results
+#' dataframe, depending on the value of argument `data`.
 #'
-#' @details  UKB showcase documentation for \href{http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=40100}{data field 40100} describes the categorical columns of the COVID-19 results dataframe as follows:
+#' @details  UKB showcase documentation for
+#' \href{http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=40100}{data field 40100}
+#' describes the categorical columns of the COVID-19 results dataframe
+#' as follows:
 #' \describe{
 #'   \item{\bold{spectype}}{Coding 1853: COVID19 test locations. Locations/methods used to generate samples for COVID19 testing.}
 #'   \item{\bold{result}}{Coding 1854: Test result. Result of a binary test.}
@@ -273,8 +309,25 @@ bio_gp <- function(project_dir, record, gp_dir = "raw/") {
 #'   \item{\bold{acute}}{Coding 12: ACE boolean. True/False boolean value.}
 #'   \item{\bold{hosaq}}{Coding 21: Yes No or Unknown. Artificial coding, generated after data collection.}
 #' }
+#' 
+#' The \code{data} option returns various parts of the COVID-19 data:
+#' "results" returns COVID-19 test results; "misc" returns blood group
+#' extracted from imputed genotype; "tppscripts" and "emisscripts"
+#' return information on the issuing of prescription medication for the
+#' TPP and EMIS suppliers respectively; "tppclinical" and
+#' "emisclinical" return data on primary care events, such as
+#' consultations, diagnoses, history, symptoms, procedures, laboratory
+#' tests and administrative information for the TPP and EMIS suppliers
+#' respectively; "codes" returns the code meanings for the categorical
+#' variables.
+#' 
+#' For details of the structure of the data, the various GP coding systems,
+#' and UKB categorical codes used for each variable see
+#' \href{https://biobank.ndph.ox.ac.uk/ukb/refer.cgi?id=3151}{Resource 3151},
+#' Primary Care Data for COVID-19 Research.
 #'
-#' @seealso \href{http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=40100}{Data field 40100},
+#' @seealso
+#' \href{http://biobank.ndph.ox.ac.uk/showcase/field.cgi?id=40100}{Data field 40100},
 #' \href{http://biobank.ndph.ox.ac.uk/showcase/exinfo.cgi?src=COVID19_tests}{COVID-19 test results data}
 #'
 #' @importFrom data.table fread
@@ -302,7 +355,32 @@ bio_covid <- function(project_dir, data = "results", covid_dir = "raw/",
     covid_results <- file.path(project_dir, covid_dir, "covid19_misc.txt")
     df <- data.table::fread(covid_results, header = TRUE, data.table = FALSE)
   }
-
+  
+  if (data == "tppscripts") {
+    covid_results <- file.path(project_dir, covid_dir,
+                               "covid19_tpp_gp_scripts.txt")
+    df <- data.table::fread(covid_results, header = TRUE, data.table = FALSE,
+                            colClasses = c(dmd_code = "character"))
+  }
+  
+  if (data == "tppclinical") {
+    covid_results <- file.path(project_dir, covid_dir,
+                               "covid19_tpp_gp_clinical.txt")
+    df <- data.table::fread(covid_results, header = TRUE, data.table = FALSE)
+  }
+  
+  if (data == "emisscripts") {
+    covid_results <- file.path(project_dir, covid_dir,
+                               "covid19_emis_gp_scripts.txt")
+    df <- data.table::fread(covid_results, header = TRUE, data.table = FALSE)
+  }
+  
+  if (data == "emisclinical") {
+    covid_results <- file.path(project_dir, covid_dir,
+                               "covid19_emis_gp_clinical.txt")
+    df <- data.table::fread(covid_results, header = TRUE, data.table = FALSE)
+  }
+  
   if (data == "codes") {
     coding_files <- list.files(path = file.path(project_dir, code_dir),
                                pattern = "coding", full.names = TRUE)
@@ -336,15 +414,20 @@ bio_covid <- function(project_dir, data = "results", covid_dir = "raw/",
 
 #' Reads record-level HES in-patient data
 #'
-#' @description Record-level hospital episode statistics (HES) in-patient information.
+#' @description Record-level hospital episode statistics (HES)
+#' in-patient information.
 #'
 #' @param project_dir Path to the enclosing directory of a UKB project.
-#' @param record A string specifying which HES records are required: "critical", "delivery", "diag", "maternity", "oper", "psych", "hesin".
-#' @param hesin_dir Path to the enclosing directory of the primary care data.
+#' @param record A string specifying which HES records are required:
+#' "critical", "delivery", "diag", "maternity", "oper", "psych",
+#' "hesin".
+#' @param hesin_dir Path to the enclosing directory of the primary care
+#' data.
 #'
 #' @return A dataframe of the requested record-level data.
 #'
-#' @seealso \href{https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=2000}{Category 2000},
+#' @seealso
+#' \href{https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=2000}{Category 2000},
 #' \href{https://biobank.ndph.ox.ac.uk/showcase/label.cgi?id=2006}{Category 2006},
 #' \href{https://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=138483}{Resource 138483},
 #' \href{https://biobank.ndph.ox.ac.uk/showcase/refer.cgi?id=141140}{Resource 141140}
@@ -395,12 +478,16 @@ bio_hesin <- function(project_dir, record, hesin_dir = "raw/") {
 #' Reads death records
 #'
 #' @param project_dir Path to the enclosing directory of a UKB project.
-#' @param record A string specifying which death data are required: "death" (default) includes date of death, "cause" includes ICD-10 cause of death.
+#' @param record A string specifying which death data are required:
+#' "death" (default) includes date of death, "cause" includes ICD-10
+#' cause of death.
 #' @param death_dir Path to the enclosing directory of the death data.
 #'
-#' @return A dataframe of including either the date of death, or cause of death.
+#' @return A dataframe of including either the date of death, or cause
+#' of death.
 #'
-#' @seealso \href{https://biobank.ctsu.ox.ac.uk/crystal/label.cgi?id=100093}{Category 100093},
+#' @seealso
+#' \href{https://biobank.ctsu.ox.ac.uk/crystal/label.cgi?id=100093}{Category 100093},
 #' \href{https://biobank.ctsu.ox.ac.uk/crystal/refer.cgi?id=134993}{Resource 134993}
 #'
 #' @importFrom data.table fread
@@ -425,7 +512,11 @@ bio_death <- function(project_dir, record = "death", death_dir = "raw/") {
 
 #' Lists project genetic directory contents
 #'
-#' @description The listed files in genotyped/ and imputed/ are symlinks to the central KCL UKB genetic data. Symlinks to the sample information files .fam and .sample are included in the genotyped/ and imputed/ directories respectively. Relatedness and sample QC files are included the imputed/ directory.
+#' @description The listed files in genotyped/ and imputed/ are
+#' symlinks to the central KCL UKB genetic data. Symlinks to the sample
+#' information files .fam and .sample are included in the genotyped/
+#' and imputed/ directories respectively. Relatedness and sample QC
+#' files are included the imputed/ directory.
 #'
 #' @param project_dir Path to the enclosing directory of a UKB project.
 #'
