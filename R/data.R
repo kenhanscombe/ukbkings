65d6abb0de26312d8c9f076e2463ce06eb3aa1f7
kenhanscombe <ken.hanscombe@gmail.com>
2022-01-26 11:39:42 +0000

    Include drug reference data and document

---
+++ R/data.R
@@ -0,0 +1,78 @@
+#' PharmGKB curated drug list
+#'
+#' A dataset containing all Chemicals with the Type of "Drug" in the PharmGKB
+#' knowledgebase. Not all of these drugs have been involved in PharmGKB
+#' annotations.
+#'
+#' @format A data frame with 3493 obs. of  24 variables:
+#' \describe{
+#'   \item{pharmgkb_accession id}{Identifier assigned to this chemical by PharmGKB}
+#'   \item{name}{Name PharmGKB uses for this drug}
+#'   \item{generic_names}{Known generic names for this drug, comma-separated and "-enclosed}
+#'   \item{trade_names}{Known trade names for this drug, comma-separated and "-enclosed}
+#'   \item{brand_mixtures}{Known brand mixtures this drug is in, comma-separated and "-enclosed}
+#'   \item{type}{Categories PharmGKB has assigned to this drug, can be more than one, possible values: Drug, Metabolite,
+#' Ion, Drug Class, Biological Intermediate, Small Molecule, Prodrug}
+#'   \item{cross_references}{References to other resources in the form "resource:id", comma-separated}
+#'   \item{smiles}{The SMILES structure for this drug}
+#'   \item{inchi}{The InCHI key for this drug}
+#'   \item{dosing_guideline}{"Yes" if PharmGKB has annotated a guideline with this drug, "No" otherwise}
+#'   \item{external_vocabulary}{Term for this drug in another vocabulary in the form "vocabulary:id", comma-separated}
+#' }
+#'
+#' Multiple "Type" values can be assigned to a given entry
+#' since the same substance can be used in different contexts. The
+#' "Type" values used for Drugs & Chemicals are as follows:
+#'
+#' * __Drug__ A chemical substance used in the treatment, cure,prevention, or diagnosis of disease.
+#' * __Metabolite__ Any intermediate or product resulting from metabolism.
+#' * __Ion__ An atomic or molecular particle having a net electric charge.
+#' * __Drug Class__ A drug class is a group of medications that may work in the same way, have a similar chemical structure, or are used to treat the same health condition.
+#' * __Biological Intermediate__ An endogenous small molucule or ion.
+#' * __Small Molecule__ An electrically neutral entity consisting of more than one atom.
+#' * __Prodrug__ A compound that must undergo chemical conversion by metabolic processes before becoming the pharmacologically active drug for which it is a prodrug.
+#'
+#' @source \url{https://www.pharmgkb.org/downloads}
+"drug_pharmgkb"
+
+
+#' Active ingredients and ATC code of UKB self-reported medications
+#'
+#' A dataset containing
+#' [Wu et al. (2019) Nat Commun](10.1038/s41467-019-09572-5),
+#' Supplementary Data 1: Active ingredients and ATC code of medications
+#' in UK Biobank. This study was a series of "GWASs of self-reported
+#' medication use from 23 medication categories".
+#'
+#' @format A data frame with 1752 obs. of  4 variables:
+#' \describe{
+#'   \item{category}{Category}
+#'   \item{ukb_code}{Coding reported by UK Biobank}
+#'   \item{atc_code}{Medication ATC code}
+#'   \item{name}{Drug name}
+#' }
+"drug_gwas"
+
+
+#' Curated dm+d antidepressant names
+#'
+#' Antidepressant dm+d names extracted by [Fabbri et al. (2021) Genetic
+#' and clinical characteristics of treatment-resistant depression using
+#' primary care records in two UK cohorts](https://pubmed.ncbi.nlm.nih.gov/33753889/)
+#' \describe{
+#'   \item{dmd_name}{Category}
+#' }
+"drug_dmd_antidep"
+
+
+#' UKB primary care drug maps and lookups
+#'
+#' A nested tibble of the UKB primary care coding lookups and maps
+#' including drug information.
+#' @source \url{}
+#' \describe{
+#'   \item{df}{Category}
+#'   \item{name}{}
+#'   \item{description}{}
+#' }
+"drug_ukb"
