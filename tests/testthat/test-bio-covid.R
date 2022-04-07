13c97bc9d848dbcd2c0538166d64f51bc5d2e252
kenhanscombe <ken.hanscombe@gmail.com>
2022-04-07 17:08:23 +0100

    Delete tests of deprecated functions

--- tests/testthat/test-bio-covid.R
+++
@@ -1,8 +0,0 @@
-context('covid-19')
-
-test_that("bio_covid stops with error if no covid-19 data available", {
-  expect_error(
-    bio_covid(".", data = "results", covid_dir = ".", code_dir = "."),
-    "COVID-19 data is not available for this project."
-  )
-})
